3f0daf15a005c1cd56ddd5b175858f19980b6421
hdowne <43477558+hdowne@users.noreply.github.com>
2023-09-08 22:20:06 -0400

    Add files via upload

--- remote_node.ino
+++ remote_node.ino
@@ -1,4 +1,23 @@
+/*
+ *  © 2023, Harold Downe <hdowne@yahoo.ca>. All rights reserved.
+ *  
+ *  This file is part of DCC++EX API
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+/* 
 
 /*
 *
@@ -10,7 +29,7 @@
 
 see https://nrf24.github.io/RF24Network/ for how to configure nodes.
 
- *         
+        
 NRF24L01 Wired as follows for uno
             uno     pro micro
 ce/cs         9
